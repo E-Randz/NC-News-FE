026219ace9eb4693e69c8835a9cd57580a118944
Emma Randall <eerandall1@gmail.com>
2019-02-05 15:47:57 +0000

    create users component

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import { fetchUsers } from '../utils';
+import User from './User';
+
 
 class Users extends Component {
   state = { 
     users: [],
+    loadingUsers: true,
+  }
+  componentDidMount () {
+    fetchUsers()
+      .then(users => {
+        this.setState({
+          users,
+          loadingUsers: false,
+        })
+      })
   }
   render() { 
+    const { loadingUsers, users} = this.state;
     return ( 
-      <div>
-        users
+      <div className='Users'>
+      {loadingUsers && <p>Loading Users...</p>}
+      {users.map(user => {
+        return (
+          <User user={user} />
+        )
+      })}
       </div>
     );
   }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,29 +1,25 @@
 import axios from 'axios'
-const base_url = 'https://northcoders-news-project.herokuapp.com/api'
+const BASE_URL = 'https://northcoders-news-project.herokuapp.com/api'
 
 export const fetchArticles = async (topic) => {
-  const url = topic ? `${base_url}/topics/${topic}/articles` : `${base_url}/articles`;
+  const url = topic ? `${BASE_URL}/topics/${topic}/articles` : `${BASE_URL}/articles`;
   const { data: { articles } } = await axios.get(url);
   return articles;
 }
 
 export const fetchOneArticle = async (article_id) => {
-  const url = `${base_url}/articles/${article_id}`;
+  const url = `${BASE_URL}/articles/${article_id}`;
   const { data : { article } } = await axios.get(url);
   return article;
 }
 
 export const fetchComments = async (article_id) => {
-  const url = `${base_url}/articles/${article_id}/comments`;
+  const url = `${BASE_URL}/articles/${article_id}/comments`;
     const { data: { comments } } = await axios.get(url)
     return comments;
-  // } catch(err) {
-  //   console.dir(err);
-  //   return err;
-  // }
 }
 export const fetchTopics = async (article_id) => {
-  const url = `${base_url}/topics`;
+  const url = `${BASE_URL}/topics`;
   try {
     const { data: { topics } } = await axios.get(url)
     return topics;
@@ -31,4 +27,10 @@ export const fetchTopics = async (article_id) => {
     console.log(err);
     return err;
   }
+}
+
+export const fetchUsers = async () => {
+  const url = `${BASE_URL}/users`;
+  const { data: { users } } = await axios.get(url)
+  return users;
 }
