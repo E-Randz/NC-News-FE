55e10f9e3005d2d4394c2b6f91cf92105677a730
Emma Randall <eerandall1@gmail.com>
2019-02-05 09:48:26 +0000

    articles page lists articles

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -15,11 +15,25 @@ class Articles extends Component {
     })
   }
   render() { 
+    const { articles } = this.state
     return ( 
       <div className='Articles'>
         <SortAndFilter />
         <div className='Articles-results'>
-
+          {articles.map(article => {
+            let timestamp = article.created_at.toString();
+            const created_at = new Date(timestamp).toString().replace(/ GMT.*/, '');
+            return (
+              <div key={article.article_id} className="Article-item">
+                <h2 className="Article-title">{article.title}</h2>
+                <p className="Article-author">{article.author}</p>
+                <p className="Article-topic">{article.topic}</p>
+                <p className="Article-createdAt">{created_at}</p>
+                <p className="Article-commentCount">{article.comment_count}</p>
+                <p className="Article-votes">Votes: {article.votes}</p>
+              </div>
+            )
+          })}
         </div>
       </div>
     );
