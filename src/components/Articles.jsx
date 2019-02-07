51e42d277bcdb074dff3a99154f4ffb5cf653d1c
Emma Randall <eerandall1@gmail.com>
2019-02-07 14:15:51 +0000

    styling on dashboard, articles and header

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -44,23 +44,27 @@ class Articles extends Component {
       <div className='Articles'>
         <SortAndFilter queries={queries} handleFilterSubmit={this.handleFilterSubmit} handleFilterChange={this.handleFilterChange} sortFields={sortFields} />
         <div className='Articles-results'>
-          <h2>Articles</h2>
+          <h1>Articles</h1>
           {topic && <h3>~{topic}~</h3>}
-          {articles.map(article => {
-            const {article_id, title, author, topic, comment_count, votes } = article;
-            let timestamp = article.created_at.toString();
-            const created_at = new Date(timestamp).toString().replace(/ GMT.*/, '');
-            return (
-              <div key={article_id} className="Article-item">
-                <h2 className="Article-title"><Link to={`/articles/${article_id}`}>{title}</Link></h2>
-                <p className="Article-author">{author}</p>
-                <p className="Article-topic">{topic}</p>
-                <p className="Article-createdAt">{created_at}</p>
-                <p className="Article-commentCount">{comment_count}</p>
-                <p className="Article-votes">Votes: {votes}</p>
-              </div>
-            )
-          })}
+          <div className='Articles-list'>
+            {articles.map(article => {
+              const {article_id, title, author, topic, comment_count, votes } = article;
+              let timestamp = article.created_at.toString();
+              const created_at = new Date(timestamp).toString().replace(/ GMT.*/, '');
+              return (
+                <div key={article_id} className="Article-item">
+                  <h2 className="Article-title"><Link to={`/articles/${article_id}`}>{title}</Link></h2>
+                  <p className="Article-author">{author}</p>
+                  <p className="Article-topic">~{topic}~</p>
+                  <div className="Article-meta">
+                    <p className="Article-commentCount">Comments: {comment_count}</p>
+                    <p className="Article-votes">Votes: {votes}</p>
+                    <p className="Article-createdAt">{created_at}</p>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
         </div>
       </div>
     );
