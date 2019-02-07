9c318c0713c9cc1942afbe277cb3a06396c0cb7f
Emma Randall <eerandall1@gmail.com>
2019-02-07 14:29:33 +0000

    topics styles

--- src/components/Topics.jsx
+++ src/components/Topics.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { fetchTopics } from '../api';
 import { Link } from '@reach/router';
+import '../styles/Topics.css';
 
 
 class Topics extends Component {
@@ -19,11 +20,12 @@ class Topics extends Component {
     const { topics } = this.state;
     return ( 
       <div className='Topics'>
+        <h1>Topics</h1>
         {topics.map(topic => {
           const { slug, description } = topic;
           return (
             <div key={slug} className="Topic-item">
-              <h2 className="Topic-slug"><Link to={`${slug}/articles`}>{slug}</Link></h2>
+              <h2 className="Topic-slug"><Link to={`${slug}/articles`}>~{slug}~</Link></h2>
               <p className="Topic-description">{description}</p>
             </div>
           )
