8dbb6ef828c94c7453a9a0a961b5f40a546c919c
Emma Randall <eerandall1@gmail.com>
2019-02-10 10:14:14 +0000

    reactored assambleQueryString util function into a reduce

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,13 +1,11 @@
 export const assembleQueryString = (sortBy, limit, sortOrder) => {
-  let queryString = '';
-  if(sortBy) {
-    queryString += `sort_by=${sortBy}&`
-  }
-  if(limit) {
-    queryString += `limit=${limit}&`
-  }
-  if(sortOrder) {
-    queryString += `sort_order=${sortOrder}&`
-  }
-  return queryString ? `?${queryString}` : '';
+  const qNames = ['sort_by', 'limit', 'sort_order'];
+  const qValues = [sortBy, limit, sortOrder];
+
+  let queries = qNames.reduce((acc, curr, i) => {
+    if (qValues[i]) acc.push(`${curr}=${qValues[i]}`);
+    return acc;
+  }, [])
+
+  return queries.length ?  `?${queries.join('&')}` : '';
 }
