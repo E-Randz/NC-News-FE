535cfa42af49223191fb2cb9f77921c3a79ad824
Emma Randall <eerandall1@gmail.com>
2019-02-17 11:40:19 +0000

    add proptypes for Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import '../styles/Header.css'
 import Logo from './Logo';
 import Nav from './Nav';
+import PropTypes from 'prop-types';
 
-const Header = ({ user, handleLogout }) => <div className="App-header">
-        <Logo />
-        <Nav handleLogout={handleLogout} user={user}/>
-      </div>
+const Header = ({ user, handleLogout }) => {
+  return(
+    <div className="App-header">
+      <Logo />
+      <Nav handleLogout={handleLogout} user={user}/>   
+    </div>
+  )
+}
 
-
+Header.propTypes = {
+  user: PropTypes.object,
+  handleLogout: PropTypes.func
+}
 
 export default Header;
