743cf04e72053f9374ea067b61442b32ffc8bdd3
Emma Randall <eerandall1@gmail.com>
2019-02-23 18:50:24 +0000

    finish pageselector component

--- src/components/PageSelector.jsx
+++ src/components/PageSelector.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const PageSelector = ({ pageCount, changePage, page }) => {
-  console.log(page);
   const pageNumbers = [];
   for (let i = 1; i <= pageCount; i++){
     pageNumbers.push(i);
@@ -9,16 +9,21 @@ const PageSelector = ({ pageCount, changePage, page }) => {
 
   return ( 
     <ul key='Page-selector' className='Page-selector'>
-      <li><button onClick={(e) => changePage(e, -1)} disabled={page === 1}>previous</button></li>
+      <li><button onClick={(e) => changePage(e, -1)} disabled={page === 1}>{'<'}</button></li>
       {pageNumbers.map((number) => {
-        console.log(number,page, page===number)
         return (
           <li key={number}><button onClick={changePage} disabled={page === number}>{number}</button></li>
         )
       })}
-      <li><button onClick={(e) => changePage(e, 1)} disabled={page === pageCount}>next</button></li>
+      <li><button onClick={(e) => changePage(e, 1)} disabled={page === pageCount}>{'>'}</button></li>
     </ul>
   );
+}
+
+PageSelector.propTypes = {
+  pageCount: PropTypes.number,
+  changePage: PropTypes.func,
+  page: PropTypes.number,
 }
  
 export default PageSelector;
