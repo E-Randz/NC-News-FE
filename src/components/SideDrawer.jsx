d4d52deda8a67932aefaebeb9aea47bccca3047e
Emma Randall <eerandall1@gmail.com>
2019-02-24 19:06:02 +0000

    require handleLogout and user in sideDrawer

--- src/components/SideDrawer.jsx
+++ src/components/SideDrawer.jsx
@@ -3,7 +3,7 @@ import { Link } from '@reach/router';
 import '../styles/SideDrawer.css';
 import PropTypes from 'prop-types';
 
-const SideDrawer = ({ show }) => {
+const SideDrawer = ({ show, handleLogout, user }) => {
   const drawerClasses = show ? 'side-drawer open' : 'side-drawer' 
   return ( 
     <nav className={drawerClasses}>
@@ -11,6 +11,7 @@ const SideDrawer = ({ show }) => {
         <li className='Nav-item'><Link to='/articles'>Articles</Link></li>
         <li className='Nav-item'><Link to='/topics'>Topics</Link></li>
         <li className='Nav-item'><Link to='/users'>Users</Link></li>
+        {user && <li className='Nav-item' onClick={handleLogout}><Link to='/'>Log out</Link></li>}
       </ul>
     </nav>
   );
@@ -18,6 +19,8 @@ const SideDrawer = ({ show }) => {
 
 SideDrawer.propTypes = {
   show: PropTypes.bool.isRequired,
+  handleLogout: PropTypes.func.isRequired,
+  user: PropTypes.object,
 }
 
  
