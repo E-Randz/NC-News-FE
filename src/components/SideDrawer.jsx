2ab28675f9b240fff2c7d1db5ab5dedf3a144725
Emma Randall <eerandall1@gmail.com>
2019-02-24 19:02:56 +0000

    add ternary class choice to side drawer to enable toggling

--- src/components/SideDrawer.jsx
+++ src/components/SideDrawer.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Link } from '@reach/router';
+import '../styles/SideDrawer.css';
+import PropTypes from 'prop-types';
 
-const SideDrawer = (props) => {
+const SideDrawer = ({ show }) => {
+  const drawerClasses = show ? 'side-drawer open' : 'side-drawer' 
   return ( 
-    <nav className='side-drawer'>
+    <nav className={drawerClasses}>
       <ul>
         <li className='Nav-item'><Link to='/articles'>Articles</Link></li>
         <li className='Nav-item'><Link to='/topics'>Topics</Link></li>
@@ -12,5 +15,10 @@ const SideDrawer = (props) => {
     </nav>
   );
 }
+
+SideDrawer.propTypes = {
+  show: PropTypes.bool.isRequired,
+}
+
  
 export default SideDrawer;
