32d79f82612d54e02b225768f337c7c566d7b6ec
Emma Randall <eerandall1@gmail.com>
2019-02-24 19:21:44 +0000

    close button to allow for close via drawer in addition to the backdrop

--- src/components/SideDrawer.jsx
+++ src/components/SideDrawer.jsx
@@ -3,14 +3,17 @@ import { Link } from '@reach/router';
 import '../styles/SideDrawer.css';
 import PropTypes from 'prop-types';
 
-const SideDrawer = ({ show, handleLogout, user }) => {
+const SideDrawer = ({ show, drawerToggleClick, handleLogout, user }) => {
   const drawerClasses = show ? 'side-drawer open' : 'side-drawer' 
   return ( 
     <nav className={drawerClasses}>
+      <div>
+        <button onClick={drawerToggleClick} className='close-button'>X</button>
+      </div>
       <ul>
-        <li className='Nav-item'><Link to='/articles'>Articles</Link></li>
-        <li className='Nav-item'><Link to='/topics'>Topics</Link></li>
-        <li className='Nav-item'><Link to='/users'>Users</Link></li>
+        <li onClick={drawerToggleClick} className='Nav-item'><Link to='/articles'>Articles</Link></li>
+        <li onClick={drawerToggleClick} className='Nav-item'><Link to='/topics'>Topics</Link></li>
+        <li onClick={drawerToggleClick} className='Nav-item'><Link to='/users'>Users</Link></li>
         {user && <li className='Nav-item' onClick={handleLogout}><Link to='/'>Log out</Link></li>}
       </ul>
     </nav>
@@ -21,6 +24,7 @@ SideDrawer.propTypes = {
   show: PropTypes.bool.isRequired,
   handleLogout: PropTypes.func.isRequired,
   user: PropTypes.object,
+  drawerToggleClick: PropTypes.func.isRequired,
 }
 
  
